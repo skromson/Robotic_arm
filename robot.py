bf50315a1742400db1afb63134ae44441ff90b7d
tymas <tym.stanczewski@gmail.com>
2026-05-27 10:27:15 +0200

    Fix error in Robot class

--- robot.py
+++ robot.py
@@ -3,33 +3,9 @@ import numpy
 #class defining robots logic
 class Robot():
     def __init__(self, phi, zHeight, rExtension):
-        self.__phi = self.phi
-        self.__zHeight = self.zHeight
-        self.__rExtension = self.rExtension
-    
-    #setting bounds to phi
-    @property.setter
-    def phi(self, angle):
-        if angle < -170:
-            set.__phi = -170
-        if angle > 170:
-            set.__phi = 170
-    
-    #setting bounds to zHeight
-    @property.setter
-    def zHeight(self, height):
-        if height < 0:
-            self.__zHeight = 0
-        if height > 100:
-            self.__zHeight = 100
-    
-    #setting bounds to rExtension
-    @property.setter
-    def rExtension(self, extension):
-        if extension < 0:
-            self.__rExtension = 0
-        if extension > 50:
-            self.__rExtension = 50
+        self.__phi = phi
+        self.__zHeight = zHeight
+        self.__rExtension = rExtension
     
     #getters
     @property
@@ -42,4 +18,27 @@ class Robot():
     def rExtension(self):
         return self.__rExtension
     
+    #setting bounds to phi
+    @phi.setter
+    def phi(self, angle):
+        if angle < -170:
+            set.__phi = -170
+        if angle > 170:
+            set.__phi = 170
+    
+    #setting bounds to zHeight
+    @zHeight.setter
+    def zHeight(self, height):
+        if height < 0:
+            self.__zHeight = 0
+        if height > 100:
+            self.__zHeight = 100
+    
+    #setting bounds to rExtension
+    @rExtension.setter
+    def rExtension(self, extension):
+        if extension < 0:
+            self.__rExtension = 0
+        if extension > 50:
+            self.__rExtension = 50
     
